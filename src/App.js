3901679a45d5fa15d3183fe7e3f79e97d70a8d58
Alex Lau <alex.wanna.nirvana@gmail.com>
2022-08-30 23:08:03 +0800

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { DateRange } from 'react-date-range';
+
+// 套件使用 css
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import './App.css'
 
 function App() {
+  const [state, setState] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: 'selection'
+    }
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>若input背景色是綠色代表是從App.js import App.css css生效</div>
+      <DateRange
+        editableDateInputs={true}
+        onChange={item => setState([item.selection])}
+        moveRangeOnFirstSelection={false}
+        ranges={state}
+      />
+
+      <div className='tailwindTest'>從tailwind設的custom class, 藍色即生效</div>
+    </>
+
+  )
 }
 
-export default App;
+export default App
